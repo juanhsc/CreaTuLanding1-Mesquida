53b19e3065faa5cf596c1468e6436c0abf776831
Juan Pedro Mesquida <Juan.Mesquida@boldt.com.ar>
2025-08-26 15:31:20 -0300

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import CartWidget from './components/CartWidget';
+import { Router,BrowserRouter,Route } from 'react-router-dom';
+
+
+
 const App = () => {
   return (
-    
+    <BrowserRouter>
     <div style={{ fontFamily: 'sans-serif', minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
      
       <NavBar />
      
       <ItemListContainer greeting="¡Bienvenido a Mi Tienda Online!" />
     </div>
+    </BrowserRouter>
   );
 };
 
