3ae7b2d4672bb256f9b4e087c2a01e5d6731890e
Juan Pedro Mesquida <Juan.Mesquida@boldt.com.ar>
2025-08-26 15:45:21 -0300

    actualizacion2

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import CartWidget from './components/CartWidget';
-import { Router,BrowserRouter,Route } from 'react-router-dom';
+import { Router,BrowserRouter,Route, Routes } from 'react-router-dom';
 
 
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div style={{ fontFamily: 'sans-serif', minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
+    
      
       <NavBar />
-     
-      <ItemListContainer greeting="¡Bienvenido a Mi Tienda Online!" />
-    </div>
+    <Routes>
+<Route path='/' element= {<ItemListContainer greeting="¡Bienvenido a Mi Tienda Online!" />}> </Route>
+<Route path="/item" element={ItemDetailContainer/>}/>
+    </Routes>
+      
+    
     </BrowserRouter>
-  );
-};
+  )
+}
 
 export default App;
